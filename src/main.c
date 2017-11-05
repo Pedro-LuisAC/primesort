ec57a36a8bf385f1e04b0a7d39b350a463fe8711
Pedro Luis <mittlesbr@gmail.com>
2017-11-05 14:53:30 -0200

    tudo funcionando

--- src/main.c
+++ src/main.c
@@ -6,12 +6,52 @@
  */
 
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+int testa_primo(int inteiro){
+	int aux_primo;
+	if(inteiro == 1){
+		return 1;
+	}
+	else if(inteiro == -1){
+		return 0; //por seguranga
+	}
+	else{
+		for(int i = 2; i <= inteiro/2; i++){
+			aux_primo = inteiro % i;
+			if(aux_primo == 0){
+				return 1;
+			}
+		}
+		return 0;
+	}
+}
 
 int main() {
+	int i,aux_primo,leitura,flag_primo;
 
-  int x, y;
+	FILE *inicia_pipe;
 
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
-  return 0;
+	inicia_pipe = popen("sort -n -r","w");
+
+	if(inicia_pipe == NULL) exit(1); //no tutorial fala para colocar isso mas nao sei pq
+
+			
+	while (leitura != -1){
+		scanf("%d",&leitura);
+		if(leitura == -1){
+			goto SAI; //so por seguranca
+		}
+		else{
+		if(testa_primo(leitura)){
+			fprintf(inicia_pipe, "%d\n",leitura);
+		}
+	}
+	}
+	SAI:
+	pclose(inicia_pipe);
+	kill(inicia_pipe); //por serguranca
+    return(0);
 }
